558e866189afb163125a31547eb790fb78554d86
Angela Clarke <angeladoust@hotmail.com>
2022-08-30 19:52:51 +1000

    json updates

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav';
 
-// importat pages
-import HomePage from './pages/HomePage'
-import ProjectPage from './pages/ProjectPage'
-import LoginPage from './pages/LoginPage';
-import ProjectForm from './components/ProjectForm';
+// import pages
+import Dashboard from './pages/Dashboard'
+import LoginPage from './pages/LoginPage'
 
-// import components
-import HeroSection from './components/HeroSection';
-import CreateAccount from './pages/CreateAccount';
+
+
 
 function App() {
 
   return (
     <Router>
       <div>
-        <Nav />
-        <HeroSection />
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/project/:id" element={<ProjectPage />} />
-          <Route path="login" element={<LoginPage/>} />
-          <Route path="CreateProject" element={<ProjectForm/>} />
-          <Route path="CreateAccount" element={<CreateAccount/>} />
+          <Route exact path="/" element={<Dashboard />} />
         </Routes>
       </div>
     </Router>
